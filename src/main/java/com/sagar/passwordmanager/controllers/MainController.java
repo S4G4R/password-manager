ceb47934ec3bc4eb4a2b19505366da94735895ae
Sagar Vrajalal <sagar7666@gmail.com>
2019-02-20 22:09:29 +0530

    add main screen UI

---
+++ src/main/java/com/sagar/passwordmanager/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.sagar.passwordmanager.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    public MainController() {
+
+    }
+
+    @FXML
+    private Label label;
+
+}
