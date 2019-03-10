338ec04ae28f4b88ff3a5e63edab96fa921e2e8c
Sagar Vrajalal <sagar7666@gmail.com>
2019-03-10 21:26:53 +0530

    add animated popup to set master password

--- src/main/java/com/sagar/passwordmanager/controllers/SplashController.java
+++ src/main/java/com/sagar/passwordmanager/controllers/SplashController.java
@@ -1,44 +1,64 @@
 package com.sagar.passwordmanager.controllers;
 
-import com.sagar.passwordmanager.management.DialogManagement;
-
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.util.Duration;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SplashController implements Initializable {
 
-    @FXML
-    private Button SetMasterPassword;
-    @FXML
-    private Button LoginButton;
+    @FXML private SplitPane SetPasswordDialog;
+
+    @FXML private Button SetMasterPassword;
+    @FXML private Button EnterMasterPassword;
+
+    private TranslateTransition transIn;
+    private TranslateTransition transOut;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         //SetMasterPassword.setDisable(true);
-        LoginButton.setDisable(true);
+        EnterMasterPassword.setDisable(true);
+
+        // Set up sliding animation
+        setUpAnimation();
+    }
+
+    private void setUpAnimation() {
+        this.transIn = new TranslateTransition(Duration.millis(250), SetPasswordDialog);
+        transIn.setToY(0);
+        this.transOut = new TranslateTransition(Duration.millis(250), SetPasswordDialog);
+
+        // Moving dialog up and away from main scene
+        SetPasswordDialog.translateYProperty().setValue(-(SetPasswordDialog.getPrefHeight()));
     }
 
     @FXML
-    private void enterMasterPassword(ActionEvent event) throws Exception {
+    private void enterMasterPassword() throws Exception {
         // TODO: VALIDATION
 
         SceneSwitcher.goToMain();
     }
 
     @FXML
-    private void setMasterPassword(ActionEvent event) {
+    private void setMasterPassword() {
+
+        if (SetPasswordDialog.getTranslateY() != 0) {
+            transIn.play();
+        } else {
+            transOut.setToY(-(SetPasswordDialog.getPrefHeight()));
+            transOut.play();
+        }
+
         // TODO: VALIDATION
-        Dialog dialog = DialogManagement.createMasterPasswordDialog();
 
-        dialog.showAndWait();
-
-        System.out.println(dialog.getResult());
     }
 
 
